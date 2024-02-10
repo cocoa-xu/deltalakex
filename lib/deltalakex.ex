18d02f023886fbd8adbd21db36e60a4df9c80700
Cocoa <i@uwucocoa.moe>
2024-02-10 22:06:57 +0800

    init

---
+++ lib/deltalakex.ex
@@ -0,0 +1,18 @@
+defmodule Deltalakex do
+  @moduledoc """
+  Documentation for `Deltalakex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Deltalakex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/deltalakex_test.exs
@@ -0,0 +1,8 @@
+defmodule DeltalakexTest do
+  use ExUnit.Case
+  doctest Deltalakex
+
+  test "greets the world" do
+    assert Deltalakex.hello() == :world
+  end
+end
